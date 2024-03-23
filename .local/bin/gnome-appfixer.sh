c455e9b009a4d247fd019201b83a0f16d4f2fc1f
Brad Thornborrow <brad@thedeskofbrad.ca>
2024-03-22 21:32:20 -0400

    Update appfixer script

--- .local/bin/gnome-appfixer.sh
+++ .local/bin/gnome-appfixer.sh
@@ -1,16 +1,13 @@
 # Makes the GNOME Shell Apps Dashboard sort the apps into their
-#   respective categories based on the FreeDesktop standard.
+#   respective categories based on the FreeDesktop standard
 
-# By using this shell script, you hereby take full responsiblity
-#   for anything that happens to your system.
-
-# Lincensed under the MIT License.
+# Licensed under the MIT License
 # Copyright (c) 2017 Ben Godfrey
 
 gsettings set org.gnome.shell app-picker-layout "[]"
 gsettings reset-recursively org.gnome.desktop.app-folders
 
-gsettings set org.gnome.desktop.app-folders folder-children "['accessories', 'console', 'games', 'media', 'network', 'office', 'system', 'utilities']"
+gsettings set org.gnome.desktop.app-folders folder-children "['accessories', 'console', 'games', 'media', 'network', 'office', 'system', 'tools']"
 
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/accessories/ name "Accessories"
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/accessories/ categories "['Utility']"
@@ -21,8 +18,8 @@ gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folder
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/games/ name "Games"
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/games/ categories "['Game']"
 
-gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/graphics/ name "Media"
-gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/graphics/ categories "['Graphics','AudioVideo','Audio']"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/media/ name "Media"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/media/ categories "['Graphics','AudioVideo','Audio']"
 
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/network/ name "Network"
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/network/ categories "['Network']"
@@ -33,5 +30,5 @@ gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folder
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/system/ name "System"
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/system/ categories "['System','Settings']"
 
-gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/utilities/ name "Utilities"
-gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/utilities/ categories "['Development']"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/tools/ name "Tools"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/tools/ categories "['Development']"
