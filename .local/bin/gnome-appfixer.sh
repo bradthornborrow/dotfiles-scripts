cb7f3982168348597a7eb85a114c7ddb2267235d
Brad Thornborrow <brad@thedeskofbrad.ca>
2024-06-07 20:29:08 -0400

    Update scripts

--- .local/bin/gnome-appfixer.sh
+++ .local/bin/gnome-appfixer.sh
@@ -5,31 +5,12 @@
 # Copyright (c) 2017 Ben Godfrey
 
 gsettings set org.gnome.shell app-picker-layout "[]"
-gsettings reset-recursively org.gnome.desktop.app-folders
 
-gsettings set org.gnome.desktop.app-folders folder-children "['accessories', 'console', 'games', 'media', 'network', 'office', 'settings']"
+# gsettings reset-recursively org.gnome.desktop.app-folders
+# gsettings set org.gnome.desktop.app-folders folder-children "['accessories', 'settings']"
 
-gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/accessories/ name "Accessories"
-gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/accessories/ categories "['Utility']"
+# gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/accessories/ name "Accessories"
+# gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/accessories/ categories "['Utility']"
 
-gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/console/ name "Console"
-gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/console/ categories "['ConsoleOnly']"
-
-gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/games/ name "Games"
-gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/games/ categories "['Game']"
-
-gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/media/ name "Media"
-gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/media/ categories "['Graphics','AudioVideo','Audio']"
-
-gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/network/ name "Network"
-gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/network/ categories "['Network']"
-
-gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/office/ name "Office"
-gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/office/ categories "['Office','Development']"
-
-# gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/system/ name "System"
-# gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/system/ categories "['System']"
-
-gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/settings/ name "Settings"
-gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/settings/ categories "['Settings','DesktopSettings']"
-
+# gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/settings/ name "Settings"
+# gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/settings/ categories "['Settings','DesktopSettings']"
--- .local/bin/webp-convert.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-for filename in *.webp ; do
-  convert $filename ${filename%.webp}.jpg
-done
