b7894bc63afbc6b7ec1e7711aa0eb2f7f950b8ec
Brad Thornborrow <brad@thedeskofbrad.ca>
2024-08-05 12:31:59 -0400

    Update dconf backups

--- .local/bin/dconf-settings.sh
+++ .local/bin/dconf-settings.sh
@@ -3,11 +3,11 @@
 case "$1" in
   "backup")
     # Backup Gnome settings
-    dconf dump / > $HOME/.config/dconf/dconf-`hostname -s`.ini
+    dconf dump / > $HOME/.config/dconf/`hostname -s`.ini
     ;;
   "restore")
     # Restore Gnome settings
-    dconf load / < $HOME/.config/dconf/dconf-`hostname -s`.ini
+    dconf load / < $HOME/.config/dconf/`hostname -s`.ini
     ;;
   *)
     echo "Please specify 'backup' or 'restore' only."
