68a00aca34b925fd968def820bdfa7b7c2defad3
Brad Thornborrow <brad@thedeskofbrad.ca>
2024-12-28 22:12:03 -0500

    Update dconf backup

--- .local/bin/dconf-settings.sh
+++ .local/bin/dconf-settings.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+HOST="$(hostname -s | tr '[:upper:]' '[:lower:]')"
 
 case "$1" in
   "backup")
     # Backup dconf settings
-    dconf dump / > $HOME/.config/dconf/`hostname -s`.ini
+    dconf dump / > $HOME/.config/dconf/$HOST.ini
     ;;
   "restore")
     # Restore dconf settings
-    dconf load / < $HOME/.config/dconf/`hostname -s`.ini
+    dconf load / < $HOME/.config/dconf/$HOST.ini
     ;;
   *)
     echo "Please specify 'backup' or 'restore' only."
