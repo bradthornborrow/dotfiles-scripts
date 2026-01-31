e411f713f11c5dbe16ee13a09999df9841c5defd
Brad Thornborrow <brad@thedeskofbrad.ca>
2026-01-31 16:30:17 -0500

    Update backup scripts

---
+++ .local/bin/back-home.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# description: Use RSYNC to backup selected local user folders
+
+# Show arguments if none entered
+if [ $# -eq 0 ]; then
+    echo "Usage: `basename $0` DST_PATH [-n]"
+    exit 0
+fi
+
+# List of user home folders for backup
+echo "Syncing folder: $HOME"
+echo rsync -rltvWh $2 --delete --exclude '.DS_Store' --exclude '.Trash*' --exclude 'Library/CloudStorage/ShellFish'  --modify-window=2 $HOME $1$HOME
+echo ""
--- .local/bin/yt-dlp.sh
+++ .local/bin/yt-dlp.sh
@@ -3,4 +3,3 @@
 # This script downloads YouTube URL as MP4 with embedded thumbnail
 #
 yt-dlp $1 -S vcodec:h264,fps,res,acodec:m4a --embed-thumbnail
-
