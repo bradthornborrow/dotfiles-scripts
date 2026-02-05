c7526a0692a812dfbbbf01df670d36969d8aadc7
Brad Thornborrow <brad@thedeskofbrad.ca>
2026-02-05 18:55:53 -0500

    Add rclone script

---
+++ .local/bin/rclone-nas.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# description: Use RSYNC to backup NAS shares
+
+# Show arguments if none entered
+if [ $# -eq 0 ]; then
+    echo "Usage: `basename $0` DST_PATH [-n]"
+    exit 0
+fi
+
+SRC=nas.local
+SRC_PATH=/tmp/$SRC
+mkdir $SRC_PATH
+
+read -sp "Enter password for NAS: " PASSWORD
+echo
+
+# URLencode password to avoid special character issues
+PASSWORD=$(echo -ne "$PASSWORD" | xxd -plain | sed 's/\(..\)/%\1/g' )
+
+# Volumes to backup
+PATHS=(Public)
+
+for VOLUME in "${PATHS[@]}"; do
+	echo "Syncing volume: $VOLUME"
+	mount -t smbfs //nas:$PASSWORD@$SRC/$VOLUME $SRC_PATH
+	if [ $? -eq 0 ]; then
+	  # rsync -rltvWh $2 --delete --modify-window=2 --exclude=.* $SRC_PATH/ $1/$VOLUME
+ 	  rclone sync $2 --exclude='.*' --modify-window=2 --multi-thread-streams=4 -P -L --metadata $SRC_PATH/ $1/$VOLUME
+      echo ""
+	  sleep 2
+	  diskutil unmount $SRC_PATH
+      echo ""
+	else
+	  echo "Mount failed, scripted exiting"
+	  exit
+	fi
+done
+
+rmdir $SRC_PATH
