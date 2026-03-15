e711ec29fa344ef477f3c8323fda51af3dfecfb1
Brad Thornborrow <brad@thedeskofbrad.ca>
2026-03-15 16:29:08 -0400

    Update whitenoise script

--- .local/bin/warp-drive.sh
+++ .local/bin/warp-drive.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-# Engage Warp Drive
+# Enterprise cabin whitenoise generator
 
 # Requires package 'sox'
 # http://www.reddit.com/r/linux/comments/n8a2k/commandline_star_trek_engine_noise_comment_from/
 
-# Enterprise original
-# play -n -c1 synth whitenoise band -n 100 20 band -n 50 20 gain +25 fade h 1 864000 1
+# Star Trek TOS engines
+# play -n -t alsa -c2 synth whitenoise band -n 100 24 band -n 300 100 gain +20
 
-# Enterprise stereo
-# play -c2 -n synth whitenoise band -n 100 24 band -n 300 100 gain +20
-
-# Enterprise TNG
+# Star Trek TNG engines
 play -n -t alsa -c1 synth whitenoise lowpass -1 120 lowpass -1 120 lowpass -1 120 gain +14
