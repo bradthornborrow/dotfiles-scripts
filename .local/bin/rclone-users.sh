6e3832753043a987aef80840ce8f84d01e35964a
Brad Thornborrow <brad@bradthornborrow.ca>
2026-07-11 21:14:21 -0400

    Update rclone scripts

--- .local/bin/rclone-users.sh
+++ .local/bin/rclone-users.sh
@@ -14,7 +14,7 @@ for FOLDER in "${PATHS[@]}"; do
 	echo "Syncing folder: $FOLDER"
 	SRC_PATH=/Users/$FOLDER/
 	DST_PATH=$1/Users/$FOLDER
-    rclone sync $2 --log-file=rclone.log --exclude-from $HOME/.local/bin/rclone.exclude --delete-during --delete-excluded --progress --skip-links $SRC_PATH $DST_PATH
+    rclone sync $2 --log-file=rclone.log --exclude-from $HOME/.local/bin/rclone.exclude --delete-during --delete-excluded --ignore-errors --progress --skip-links $SRC_PATH $DST_PATH
     echo ""
 	sleep 2
 done
